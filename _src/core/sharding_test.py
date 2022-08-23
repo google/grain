92277cfcf6ebcff6e79dbb31a2c4a538a01a2337
Mostafa Dehghani <dehghani@google.com>
2022-08-23 12:27:03 -0700

    Skip calling `_batch_fn` if it's set to None.
    PiperOrigin-RevId: 469525905

---
+++ _src/core/sharding_test.py
@@ -0,0 +1,35 @@
+"""Tests for sharding."""
+from absl.testing import absltest
+from absl.testing import parameterized
+from grain._src.core import sharding
+
+
+class ShardingTest(parameterized.TestCase):
+
+  @parameterized.parameters(
+      # num_examples, shard_index, shard_count, drop_remainder, expected_output.
+      (9, 0, 1, True, (0, 9)),
+      (9, 0, 2, True, (0, 4)),
+      (9, 1, 2, True, (4, 8)),  # Last example gets dropped.
+      (9, 0, 3, True, (0, 3)),
+      (9, 1, 3, True, (3, 6)),
+      (9, 2, 3, True, (6, 9)),
+      (9, 0, 1, False, (0, 9)),
+      (9, 0, 2, False, (0, 5)),  # First shard gets an extra example.
+      (9, 1, 2, False, (5, 9)),
+      (8, 0, 3, False, (0, 3)),  # First 2 shards get 1 example each.
+      (8, 1, 3, False, (3, 6)),
+      (8, 2, 3, False, (6, 8)),
+  )
+  def test_sharding(self, num_examples: int, shard_index: int, shard_count: int,
+                    drop_remainder, expected_output: tuple[int, int]):
+    shard_options = sharding.ShardOptions(
+        shard_index=shard_index,
+        shard_count=shard_count,
+        drop_remainder=drop_remainder)
+    actual_output = sharding.even_split(num_examples, shard_options)
+    self.assertEqual(actual_output, expected_output)
+
+
+if __name__ == '__main__':
+  absltest.main()
---
+++ _src/tensorflow/data_loaders_test.py
@@ -0,0 +1,95 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit tests for the data_loaders module."""
+from typing import Mapping, Optional
+from unittest import mock
+from absl.testing import parameterized
+
+from grain._src.core import sharding
+from grain._src.core.constants import DATASET_INDEX, RECORD_KEY  # pylint: disable=g-multiple-import
+from grain._src.tensorflow import data_loaders
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+
+def _dict_to_dataset(d: Mapping[str, np.ndarray]) -> tf.data.Dataset:
+  return tf.data.Dataset.from_tensor_slices(d)
+
+
+def _dataset_to_dict(ds: tf.data.Dataset) -> Mapping[str, np.ndarray]:
+  assert ds.cardinality() > 0 and ds.cardinality() < 1000
+  return next(ds.batch(1000).as_numpy_iterator())
+
+
+class DataLoadersTest(tf.test.TestCase, parameterized.TestCase):
+  """Tests for the data_loaders module."""
+
+  def test_add_global_record_key(self):
+    # Let say we have 3 datasets with 3, 8, 5 records each.
+    records_per_dataset = (3, 8, 5)
+    num_records = sum(records_per_dataset)
+    index = {
+        RECORD_KEY: np.concatenate((range(3), range(8), range(5)), axis=0),
+        DATASET_INDEX: np.asarray(3 * [0] + 8 * [1] + 5 * [2], np.int64),
+    }
+    index_ds = _dict_to_dataset(index)
+    actual_ds = data_loaders._add_global_record_key(
+        index_ds,
+        records_per_dataset=records_per_dataset,
+        output_key="my_output_key")
+    actual = _dataset_to_dict(actual_ds)
+    expected = index | {"my_output_key": np.arange(num_records, dtype=np.int64)}
+    self.assertAllClose(actual, expected)
+
+  @mock.patch.object(tfds.core, "DatasetInfo")
+  def test_load_from_tfds_invalid_args(self, tfds_info_mock):
+    # Neither name nor tfds_info.
+    with self.assertRaisesRegex(ValueError,
+                                "Please provide either `name` or `tfds_info`."):
+      data_loaders.load_from_tfds(
+          split="train", shard_options=sharding.NoSharding())
+    # Name and tfds_info.
+    with self.assertRaisesRegex(ValueError,
+                                "Please provide either `name` or `tfds_info`."):
+      data_loaders.load_from_tfds(
+          name="my_dataset",
+          split="train",
+          tfds_info=tfds_info_mock,
+          shard_options=sharding.NoSharding())
+
+  @parameterized.parameters([
+      (True, (12, 34), None),
+      (True, (12, 34), 7),
+      (False, (12, 34), 7),
+  ])
+  def test_load_from_tfds_sampler(self, shuffle: bool, seed: tuple[int, int],
+                                  num_epochs: Optional[int]):
+    with mock.patch.object(tfds.core, "DatasetInfo") as tfds_info_mock:
+      tfds_info_mock.file_format = (
+          tfds.core.file_adapters.FileFormat.ARRAY_RECORD)
+      loader = data_loaders.load_from_tfds(
+          tfds_info=tfds_info_mock,
+          split="train",
+          shard_options=sharding.NoSharding(),
+          seed=seed,
+          num_epochs=num_epochs,
+          shuffle=shuffle)
+      self.assertEqual(loader.sampler.shuffle, shuffle)
+      self.assertEqual(loader.sampler.seed, seed)
+      self.assertEqual(loader.sampler.num_epochs, num_epochs)
+
+
+if __name__ == "__main__":
+  tf.test.main()
