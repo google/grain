f9e4f15eaece6857c3ac873a31c31b2acfaec510
Marvin Ritter <marvinritter@google.com>
2023-07-10 10:04:30 -0700

    Add LazyDataset to public API (experimental).
    PiperOrigin-RevId: 546904945

---
+++ python_lazy_dataset.py
@@ -0,0 +1,43 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Experimental PyGrain APIs."""
+
+# pylint: disable=g-importing-member
+# pylint: disable=g-multiple-import
+# pylint: disable=unused-import
+# pylint: disable=wildcard-import
+
+from ._src.python.lazy_dataset.data_sources import SourceLazyMapDataset
+from ._src.python.lazy_dataset.lazy_dataset import (
+    LazyMapDataset,
+    LazyIterDataset,
+    LazyDatasetIterator,
+)
+from ._src.python.lazy_dataset.transformations.batch import (
+    BatchLazyMapDataset,
+    BatchLazyIterDataset,
+)
+from ._src.python.lazy_dataset.transformations.filter import (
+    FilterLazyMapDataset,
+    FilterLazyIterDataset,
+)
+from ._src.python.lazy_dataset.transformations.flatmap import FlatMapLazyMapDataset
+from ._src.python.lazy_dataset.transformations.map import (
+    MapLazyMapDataset,
+    MapLazyIterDataset,
+)
+from ._src.python.lazy_dataset.transformations.mix import MixedLazyMapDataset
+from ._src.python.lazy_dataset.transformations.ragged_batch import RaggedBatchLazyMapDataset
+from ._src.python.lazy_dataset.transformations.shuffle import ShuffleLazyMapDataset
+from ._src.python.lazy_dataset.transformations.slice import SliceLazyMapDataset
