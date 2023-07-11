addf3e9e6e5a16fa07154541ecd5264023376c42
Grain Team <grain-dev@google.com>
2023-07-11 08:53:29 -0700

    Internal
    PiperOrigin-RevId: 547203566

--- python_lazy_dataset.py
+++ python_lazy_dataset.py
@@ -19,10 +19,12 @@
 # pylint: disable=wildcard-import
 
 from ._src.python.lazy_dataset.data_sources import SourceLazyMapDataset
+from ._src.python.lazy_dataset.grain_pool_v2 import GrainPool
 from ._src.python.lazy_dataset.lazy_dataset import (
     LazyMapDataset,
     LazyIterDataset,
     LazyDatasetIterator,
+    RangeLazyMapDataset,
 )
 from ._src.python.lazy_dataset.transformations.batch import (
     BatchLazyMapDataset,
