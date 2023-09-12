aa49f2ef953d50fc56f41c0dd7c00e8235a8c312
Grain Team <grain-dev@google.com>
2023-09-12 09:44:22 -0700

    Internal
    PiperOrigin-RevId: 564749644

--- grain/_src/python/lazy_dataset/transformations/slice_test.py
+++ grain/_src/python/lazy_dataset/transformations/slice_test.py
@@ -23,15 +23,50 @@ import grain._src.python.lazy_dataset.transformations.slice as slice_ds
 
 class SliceLazyMapDatasetTest(parameterized.TestCase):
 
+  def setUp(self):
+    super().setUp()
+    self.data_len = 20
+    self.range_ds = lazy_dataset.RangeLazyMapDataset(self.data_len)
+    self.range_py_list = list(range(self.data_len))
+
+  @parameterized.parameters(
+      (0, 1, 20),
+      (0, 2, 10),
+      (1, 2, 10),
+      (0, 3, 7),
+      (1, 3, 7),
+      (2, 3, 6),
+  )
+  def test_len(self, start: int, step: int, expected_len: int):
+    range_ds_for_process = slice_ds.SliceLazyMapDataset(
+        parent=self.range_ds,
+        start=start,
+        stop=self.data_len,
+        step=step,
+    )
+    self.assertLen(range_ds_for_process, expected_len)
+
   @parameterized.parameters(
       itertools.product(range(-8, 8), range(-9, 8), [-2, -1, 1, 2])
   )
-  def test_matches_python_slicing(self, start: int, stop: int, step: int):
-    expected = list(range(6))[start:stop:step]
-    ds = lazy_dataset.RangeLazyMapDataset(6)
-    ds = slice_ds.SliceLazyMapDataset(ds, start=start, stop=stop, step=step)
-    self.assertLen(ds, len(expected))
+  def test_getitem(self, start: int, stop: int, step: int):
+    expected = self.range_py_list[start:stop:step]
+    ds = slice_ds.SliceLazyMapDataset(
+        self.range_ds, start=start, stop=stop, step=step
+    )
     actual = [ds[i] for i in range(len(ds))]
+    self.assertSequenceEqual(actual, expected)
+
+  @parameterized.parameters(
+      itertools.product(range(-8, 8), range(-9, 8), [-2, -1, 1, 2])
+  )
+  def test_iter(self, start: int, stop: int, step: int):
+    expected = self.range_py_list[start:stop:step]
+    ds = slice_ds.SliceLazyMapDataset(
+        self.range_ds, start=start, stop=stop, step=step
+    )
+    ds_iter = iter(ds)
+    actual = list(ds_iter)
     self.assertSequenceEqual(actual, expected)
 
 
