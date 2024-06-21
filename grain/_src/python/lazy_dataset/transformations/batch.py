c7649c11ceb3bfdd0f2c38a2083a59a621509e84
Grain Team <grain-dev@google.com>
2024-06-21 10:55:46 -0700

    Improve formatting of error message when shapes are mismatched during batching
    PiperOrigin-RevId: 645439749

--- grain/_src/python/lazy_dataset/transformations/batch.py
+++ grain/_src/python/lazy_dataset/transformations/batch.py
@@ -15,51 +15,61 @@
 
 from collections.abc import Sequence
 import math
-from typing import TypeVar
+import pprint
+from typing import Callable, Generic, TypeVar
 
 from grain._src.core import tree
 from grain._src.python.lazy_dataset import lazy_dataset
 import numpy as np
 
 T = TypeVar("T")
+S = TypeVar("S")
 
 
-def _make_batch(values: Sequence[T]) -> T:
+class _MakeBatch(Generic[S, T]):
   """Returns a batch of values with a new batch dimension at the front."""
-  if not values:
-    raise ValueError("Cannot batch 0 values. Please file a bug.")
 
-  try:
-    return tree.map_structure(lambda *xs: np.stack(xs), *values)
+  def __call__(self, values: Sequence[S]) -> T:
+    if not values:
+      raise ValueError("Cannot batch 0 values. Please file a bug.")
 
-  except ValueError as e:
-    # NumPy error message doesn't include actual shapes and dtypes. Provide a
-    # more helpful error message.
-    element_specs = tree.map_structure(
-        lambda x: f"{np.asarray(x).dtype}{list(np.asarray(x).shape)}", values
-    )
-    element_specs = "\n".join([str(x) for x in element_specs])
-    raise ValueError(
-        "Expected all input elements to have the same structure but got:\n"
-        f"{element_specs}"
-    ) from e
+    try:
+      return tree.map_structure(lambda *xs: np.stack(xs), *values)
+
+    except ValueError as e:
+      # NumPy error message doesn't include actual shapes and dtypes. Provide a
+      # more helpful error message.
+      element_specs = tree.map_structure(
+          lambda x: f"{np.asarray(x).dtype}{list(np.asarray(x).shape)}", values
+      )
+      raise ValueError(
+          "Expected all input elements to have the same structure but got:\n"
+          f"{pprint.pformat(element_specs)}"
+      ) from e
 
 
-class _BatchLazyDatasetIterator(lazy_dataset.LazyDatasetIterator[T]):
+_make_batch = _MakeBatch()
+
+
+class _BatchLazyDatasetIterator(
+    lazy_dataset.LazyDatasetIterator[S], Generic[T, S]
+):
   """Iterator that batches elements."""
 
   def __init__(
       self,
-      parent: lazy_dataset.LazyDatasetIterator,
+      parent: lazy_dataset.LazyDatasetIterator[S],
       batch_size: int,
       drop_remainder: bool,
+      batch_fn: Callable[[Sequence[S]], T],
   ):
     super().__init__()
     self._parent = parent
     self._batch_size = batch_size
     self._drop_remainder = drop_remainder
+    self._batch_fn = batch_fn
 
-  def __next__(self):
+  def __next__(self) -> T:
     values = []
     for _ in range(self._batch_size):
       try:
@@ -70,7 +80,7 @@ class _BatchLazyDatasetIterator(lazy_dataset.LazyDatasetIterator[T]):
         break
     if not values:
       raise StopIteration
-    return _make_batch(values)
+    return self._batch_fn(values)
 
   def get_state(self):
     return self._parent.get_state()
@@ -87,7 +97,7 @@ class _BatchLazyDatasetIterator(lazy_dataset.LazyDatasetIterator[T]):
 
 
 @lazy_dataset.lazy_map_dataset_function("batch")
-class BatchLazyMapDataset(lazy_dataset.LazyMapDataset[T]):
+class BatchLazyMapDataset(lazy_dataset.LazyMapDataset[T], Generic[T, S]):
   """Batch transformation for non-sparse LazyMapDatasets."""
 
   def __init__(
@@ -95,10 +105,12 @@ class BatchLazyMapDataset(lazy_dataset.LazyMapDataset[T]):
       parent: lazy_dataset.LazyMapDataset,
       batch_size: int,
       drop_remainder: bool = False,
+      batch_fn: Callable[[Sequence[S]], T] | None = None,
   ):
     super().__init__(parent)
     self._batch_size = batch_size
     self._drop_remainder = drop_remainder
+    self._batch_fn = _MakeBatch() if batch_fn is None else batch_fn
     if self._drop_remainder:
       self._length = len(self._parent) // self._batch_size
     else:
@@ -107,7 +119,7 @@ class BatchLazyMapDataset(lazy_dataset.LazyMapDataset[T]):
   def __len__(self):
     return self._length
 
-  def __getitem__(self, index):
+  def __getitem__(self, index) -> T:
     if isinstance(index, slice):
       return self.slice(index)
     # Each epoch gets batched separately. If users want to batch across epochs
@@ -120,7 +132,7 @@ class BatchLazyMapDataset(lazy_dataset.LazyMapDataset[T]):
     start += epoch * len(self._parent)
     stop += epoch * len(self._parent)
     values = [self._parent[i] for i in range(start, stop)]
-    return _make_batch(values)
+    return self._batch_fn(values)
 
   def __str__(self) -> str:
     return (
@@ -131,7 +143,7 @@ class BatchLazyMapDataset(lazy_dataset.LazyMapDataset[T]):
 
 
 @lazy_dataset.lazy_iter_dataset_function("batch")
-class BatchLazyIterDataset(lazy_dataset.LazyIterDataset[T]):
+class BatchLazyIterDataset(lazy_dataset.LazyIterDataset[S], Generic[S, T]):
   """Batch transformation for LazyIterDatasets."""
 
   def __init__(
@@ -139,15 +151,20 @@ class BatchLazyIterDataset(lazy_dataset.LazyIterDataset[T]):
       parent: lazy_dataset.LazyIterDataset,
       batch_size: int,
       drop_remainder: bool = False,
+      batch_fn: Callable[[Sequence[S]], T] | None = None,
   ):
     super().__init__(parent)
     self._batch_size = batch_size
     self._drop_remainder = drop_remainder
+    self._batch_fn = _MakeBatch() if batch_fn is None else batch_fn
 
-  def __iter__(self) -> _BatchLazyDatasetIterator[T]:
+  def __iter__(self) -> _BatchLazyDatasetIterator[S, T]:
     parent_iter = self._parent.__iter__()
     return _BatchLazyDatasetIterator(
-        parent_iter, self._batch_size, drop_remainder=self._drop_remainder
+        parent_iter,
+        self._batch_size,
+        drop_remainder=self._drop_remainder,
+        batch_fn=self._batch_fn,
     )
 
   def __str__(self) -> str:
