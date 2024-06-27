cb1c2413f6d7aa4dc6eca272d3ebef5a835f184c
Andrew Audibert <aaudibert@google.com>
2024-06-27 13:01:37 -0700

    Add index validation tests for index_shuffle_module
    PiperOrigin-RevId: 647418857

--- grain/_src/python/experimental/index_shuffle/python/index_shuffle_module.cc
+++ grain/_src/python/experimental/index_shuffle/python/index_shuffle_module.cc
@@ -1,15 +1,27 @@
 #include <pybind11/pybind11.h>
 
+#include <cstdint>
+
+#include "third_party/absl/strings/str_format.h"
 #include "grain/_src/python/experimental/index_shuffle/index_shuffle.h"
+#include "third_party/pybind11/include/pybind11/pybind11.h"
 
 namespace py = pybind11;
+
+uint64_t index_shuffle(uint64_t index, uint64_t max_index, uint32_t seed,
+                       uint32_t rounds) {
+  if (index > max_index) {
+    throw py::value_error(absl::StrFormat("index must be in [0, %d] but was %d",
+                                          max_index, index));
+  }
+  return ::grain::random::index_shuffle(index, max_index, seed, rounds);
+}
 
 PYBIND11_MODULE(index_shuffle_module, m) {
   constexpr char kDoc[] =
       "Returns the position of `index` in a permutation of [0, ..., "
       "max_index].";
   m.doc() = kDoc;
-  m.def("index_shuffle", &::grain::random::index_shuffle, kDoc,
-        py::arg("index"), py::arg("max_index"), py::arg("seed"),
-        py::arg("rounds"));
+  m.def("index_shuffle", &index_shuffle, kDoc, py::arg("index"),
+        py::arg("max_index"), py::arg("seed"), py::arg("rounds"));
 }
