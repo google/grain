aaee1d92475f133881da37aa77f21d9da6cb698e
Vineeth Yeevani <vineeth.yeevani@gmail.com>
2024-10-01 12:07:03 -0700

    - support for bazelmod - building wheels with bazel - support for macOS via not standard array_record library - support for ci tagging and versioning

--- grain/_src/python/experimental/index_shuffle/python/index_shuffle_module.cc
+++ grain/_src/python/experimental/index_shuffle/python/index_shuffle_module.cc
@@ -1,4 +1,21 @@
+// #include <pybind11/pybind11.h>
+
+// #include "grain/_src/python/experimental/index_shuffle/index_shuffle.h"
+
+// namespace py = pybind11;
+
+// PYBIND11_MODULE(index_shuffle_module, m) {
+//   constexpr char kDoc[] =
+//       "Returns the position of `index` in a permutation of [0, ..., "
+//       "max_index].";
+//   m.doc() = kDoc;
+//   m.def("index_shuffle", &::grain::random::index_shuffle, kDoc,
+//         py::arg("index"), py::arg("max_index"), py::arg("seed"),
+//         py::arg("rounds"));
+// }
+
 #include <pybind11/pybind11.h>
+#include <pybind11/functional.h>  // Include this if you use std::function in bindings
 
 #include "grain/_src/python/experimental/index_shuffle/index_shuffle.h"
 
@@ -9,7 +26,10 @@ PYBIND11_MODULE(index_shuffle_module, m) {
       "Returns the position of `index` in a permutation of [0, ..., "
       "max_index].";
   m.doc() = kDoc;
-  m.def("index_shuffle", &::grain::random::index_shuffle, kDoc,
-        py::arg("index"), py::arg("max_index"), py::arg("seed"),
-        py::arg("rounds"));
+  m.def("index_shuffle", [](int64_t index, int64_t max_index, int64_t seed, int64_t rounds) {
+    py::gil_scoped_release release;  // Release the GIL
+    auto result = ::grain::random::index_shuffle(index, max_index, seed, rounds);
+    py::gil_scoped_acquire acquire;  // Re-acquire the GIL before returning to Python
+    return result;
+  }, kDoc, py::arg("index"), py::arg("max_index"), py::arg("seed"), py::arg("rounds"));
 }
