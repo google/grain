dbdbf2f8bdefe5932a94d77eb09f26871d1a0711
Aireen Mei <aireenmei@google.com>
2025-04-03 10:54:58 -0700

    Add multi-file test to parquet_dataset_test
    PiperOrigin-RevId: 743624200

--- grain/_src/python/dataset/sources/parquet_dataset_test.py
+++ grain/_src/python/dataset/sources/parquet_dataset_test.py
@@ -8,10 +8,38 @@ import pyarrow.parquet as pq
 flags.FLAGS.mark_as_parsed()
 
 SOME_TEXT = [
-    "This is the first record",
-    "This is the second record",
-    "This is the third record",
-    "This is the forth record",
+    [
+        "This is the first file the first record",
+        "This is the first file the second record",
+        "This is the first file the third record",
+        "This is the first file the forth record",
+    ],
+    [
+        "This is the second file the first record",
+        "This is the second file the second record",
+        "This is the second file the third record",
+        "This is the second file the forth record",
+    ],
+]
+INTERLEAVED_TEXT = [
+    "This is the first file the first record",
+    "This is the second file the first record",
+    "This is the first file the second record",
+    "This is the second file the second record",
+    "This is the first file the third record",
+    "This is the second file the third record",
+    "This is the first file the forth record",
+    "This is the second file the forth record",
+]
+WINDOWSHUFFLED_TEXT = [
+    "This is the first file the second record",
+    "This is the second file the first record",
+    "This is the first file the first record",
+    "This is the first file the third record",
+    "This is the second file the second record",
+    "This is the second file the third record",
+    "This is the second file the forth record",
+    "This is the first file the forth record",
 ]
 
 
@@ -19,19 +47,39 @@ class ParquetIterDatasetTest(absltest.TestCase):
 
   def setUp(self):
     super().setUp()
-    temp_file = self.create_tempfile()
-    self.filename = temp_file.full_path
-    table = pa.table({"text": SOME_TEXT})
-    pq.write_table(table, self.filename, row_group_size=2)
+    self.filenames = []
+    for i in range(len(SOME_TEXT)):
+      temp_file = self.create_tempfile()
+      filename = temp_file.full_path
+      self.filenames.append(filename)
+      table = pa.table({"text": SOME_TEXT[i]})
+      pq.write_table(table, filename, row_group_size=2)
 
   def test_read_row_group(self):
-    dataset = parquet_dataset.ParquetIterDataset(self.filename)
+    dataset = parquet_dataset.ParquetIterDataset(self.filenames[0])
     records = list(dataset)
-    self.assertSequenceEqual(records, [{"text": x} for x in SOME_TEXT])
+    self.assertSequenceEqual(records, [{"text": x} for x in SOME_TEXT[0]])
 
   def test_checkpointing(self):
-    dataset = parquet_dataset.ParquetIterDataset(self.filename)
+    dataset = parquet_dataset.ParquetIterDataset(self.filenames[0])
     grain.experimental.assert_equal_output_after_checkpoint(dataset)
+
+  def test_sharded_files_and_interleaved_dataset(self):
+    dataset = grain.MapDataset.source(self.filenames)
+    dataset = dataset.map(parquet_dataset.ParquetIterDataset)
+    dataset = grain.experimental.InterleaveIterDataset(
+        dataset, cycle_length=len(self.filenames)
+    )
+    self.assertSequenceEqual(
+        list(iter(dataset)), [{"text": x} for x in INTERLEAVED_TEXT]
+    )
+
+    dataset = grain.experimental.WindowShuffleIterDataset(
+        dataset, window_size=3, seed=42
+    )
+    self.assertSequenceEqual(
+        list(iter(dataset)), [{"text": x} for x in WINDOWSHUFFLED_TEXT]
+    )
 
 
 if __name__ == "__main__":
