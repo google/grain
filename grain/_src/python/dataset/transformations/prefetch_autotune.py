2e39d771cad9d22acc62a524c92c4f69fc5751c7
Grain Team <grain-dev@google.com>
2025-08-08 09:58:04 -0700

    internal change
    PiperOrigin-RevId: 792664363

--- grain/_src/python/dataset/transformations/prefetch_autotune.py
+++ grain/_src/python/dataset/transformations/prefetch_autotune.py
@@ -2,6 +2,7 @@
 
 import dataclasses
 import itertools
+import math
 import multiprocessing
 import sys
 
@@ -32,6 +33,40 @@ def _get_element_size_bytes(element):
   return size
 
 
+def _get_average_element_size_mb(
+    dss: list[dataset.IterDataset],
+    samples_to_check: int = 5,
+):
+  """Calculates the average size of elements in the given datasets.
+
+  Args:
+    dss: A list of datasets to sample from.
+    samples_to_check: The number of samples to check to estimate element size.
+
+  Returns:
+    The average size of elements in megabytes.
+  """
+  if not dss:
+    return 0
+  samples_per_ds = math.ceil(float(samples_to_check) / len(dss))
+  # Sample elements to measure their size.
+  elements_to_sample = []
+  elements_to_sample.extend(
+      itertools.chain.from_iterable(
+          map(lambda ds: itertools.islice(ds, samples_per_ds), dss)
+      )
+  )
+  if len(elements_to_sample) < samples_to_check:
+    logging.warning("Warning: Not enough elements to sample.")
+    return 0
+
+  avg_size_bytes = np.mean(
+      [_get_element_size_bytes(e) for e in elements_to_sample]
+  )
+  avg_size_mb = float(avg_size_bytes) / (1024 * 1024)
+  return avg_size_mb
+
+
 def _get_num_workers(
     ds: dataset.IterDataset,
     *,
@@ -40,28 +75,19 @@ def _get_num_workers(
     samples_to_check: int = 5,
 ):
   """Analyzes element size to choose an optimal number of workers, then creates a MultiprocessPrefetchIterDataset."""
+  average_elem_size_mb = _get_average_element_size_mb(
+      [ds], samples_to_check=samples_to_check
+  )
   if max_workers is None:
     # MultiprocessPrefetchIterDataset defaults to using all CPUs.
     max_workers = cpu_count()
 
-  # Sample elements to measure their size.
-  elements_to_sample = list(itertools.islice(ds, samples_to_check))
-  if len(elements_to_sample) < samples_to_check:
-    logging.warning(
-        "Warning: Not enough elements to sample. Defaulting to max workers."
-    )
-    return max_workers
-
-  avg_size_bytes = np.mean(
-      [_get_element_size_bytes(e) for e in elements_to_sample]
-  )
-  avg_size_mb = float(avg_size_bytes) / (1024 * 1024)
-  if avg_size_mb <= 0:
+  if average_elem_size_mb <= 0:
     logging.warning(
         "Warning: Average element size is zero. Defaulting to max workers."
     )
     return max_workers
-  num_workers = int(ram_budget_mb / avg_size_mb)
+  num_workers = int(ram_budget_mb / average_elem_size_mb)
 
   return min(num_workers, max_workers)
 
@@ -98,5 +124,5 @@ def pick_performance_config(
   return PerformanceConfig(
       multiprocessing_options=grain_options.MultiprocessingOptions(
           num_workers=num_workers
-      )
+      ),
   )
--- grain/_src/python/dataset/transformations/prefetch_autotune_test.py
+++ grain/_src/python/dataset/transformations/prefetch_autotune_test.py
@@ -47,9 +47,12 @@ class PrefetchAutotuneTest(absltest.TestCase):
         ds=ds,
         ram_budget_mb=1024,
         max_workers=None,  # Should default to cpu_count
+        max_buffer_size=None,
     )
     mock_cpu_count.assert_called_once()
     self.assertEqual(performance_config.multiprocessing_options.num_workers, 10)
+    self.assertEqual(performance_config.read_options.prefetch_buffer_size, 10)
+    self.assertEqual(performance_config.read_options.num_threads, 16)
 
   def test_autotune_respects_max_workers_cap(self):
     # 10 MB per element. Budget allows for 102 workers, but cap is 8.
@@ -60,6 +63,7 @@ class PrefetchAutotuneTest(absltest.TestCase):
         ds=ds,
         ram_budget_mb=1024,
         max_workers=8,
+        max_buffer_size=None,
     )
     # The calculated value (102) is capped at max_workers (8).
     self.assertEqual(performance_config.multiprocessing_options.num_workers, 8)
@@ -75,6 +79,7 @@ class PrefetchAutotuneTest(absltest.TestCase):
         ds=ds,
         ram_budget_mb=1024,
         max_workers=None,
+        max_buffer_size=None,
     )
     # Should default to cpu_count when sampling fails.
     mock_cpu_count.assert_called_once()
@@ -109,6 +114,7 @@ class PrefetchAutotuneTest(absltest.TestCase):
     ds = dataset.MapDataset.source([1, 2, 3]).to_iter_dataset()
 
     # Mock _get_num_workers to avoid actual calculation.
+    # Mock _get_num_workers to avoid actual calculation.
     with mock.patch.object(
         prefetch_autotune, '_get_num_workers', return_value=4
     ) as mock_get_max:
@@ -116,10 +122,53 @@ class PrefetchAutotuneTest(absltest.TestCase):
           ds=ds,
           ram_budget_mb=512,
           max_workers=8,
+          max_buffer_size=None,
       )
       mock_get_max.assert_called_once()
 
     self.assertEqual(performance_config.multiprocessing_options.num_workers, 4)
+
+  def test_get_buffer_size_calculates_correctly(self):
+    # Each element is 10MB
+    element = np.zeros(10 * 1024 * 1024, dtype=np.uint8)
+    ds = dataset.MapDataset.source([element] * 10).to_iter_dataset()
+
+    # With a 100MB budget and 10MB elements, we expect buffer size of 10.
+    read_options = prefetch_autotune._get_buffer_size(
+        ds,
+        ram_budget_mb=100,
+        max_buffer_size=10,
+        samples_to_check=5,
+    )
+    self.assertEqual(read_options.num_threads, 16)
+    self.assertEqual(read_options.prefetch_buffer_size, 10)
+
+  def test_get_buffer_size_respects_max_threads_cap(self):
+    element = np.zeros(10 * 1024 * 1024, dtype=np.uint8)
+    ds = dataset.MapDataset.source([element] * 10).to_iter_dataset()
+
+    # Calculation would yield buffer size of 10, but it should be capped at 3.
+    read_options = prefetch_autotune._get_buffer_size(
+        ds,
+        ram_budget_mb=100,
+        max_buffer_size=3,
+        samples_to_check=5,
+    )
+    self.assertEqual(read_options.num_threads, 16)
+    self.assertEqual(read_options.prefetch_buffer_size, 3)
+
+  def test_get_buffer_size(self):
+    element = np.zeros(10 * 1024 * 1024, dtype=np.uint8)
+    ds = dataset.MapDataset.source([element] * 5).to_iter_dataset()
+
+    read_options = prefetch_autotune._get_buffer_size(
+        ds,
+        ram_budget_mb=500,
+        max_buffer_size=None,
+        samples_to_check=3,
+    )
+    self.assertEqual(read_options.num_threads, 16)
+    self.assertEqual(read_options.prefetch_buffer_size, 50)
 
   def test_find_prefetch_iter_dataset_parent_returns_parent(self):
     ds = (
@@ -177,7 +226,7 @@ class PrefetchAutotuneTest(absltest.TestCase):
     )
     self.assertEqual(list(parents_prefetch_ds[2]), [3, 4, 5, 6, 7])
 
-  def test_find_prefetch_iter_dataset_parent_multiple_parents_and_none(self):
+  def test_find_prefetch_iter_dataset_parent_multiple_parents_with_empty(self):
     ds1 = (
         dataset.MapDataset.source([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
         .map(lambda x: x + 1)
@@ -203,6 +252,50 @@ class PrefetchAutotuneTest(absltest.TestCase):
     self.assertEqual(
         list(parents_prefetch_ds[1]), [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     )
+
+  def test_get_average_element_size_mb_returns_zero_for_empty_dataset(self):
+    self.assertEqual(prefetch_autotune._get_average_element_size_mb([]), 0)
+
+  def test_get_average_element_size_mb(self):
+    element1 = np.zeros(1 * 1024 * 1024, dtype=np.uint8)  # 1MB
+    element2 = np.zeros(3 * 1024 * 1024, dtype=np.uint8)  # 3MB
+    element3 = np.zeros(5 * 1024 * 1024, dtype=np.uint8)  # 5MB
+    ds1 = dataset.MapDataset.source(
+        [element1, element2, element3]
+    ).to_iter_dataset()
+    ds2 = dataset.MapDataset.source([element1] * 10).to_iter_dataset()
+    ds3 = dataset.MapDataset.source([element2, element3] * 5).to_iter_dataset()
+
+    # samples_to_check = 5. samples_per_ds = ceil(5/3) = 2.
+    # ds1: 2 samples of size (1 and 3). Total = 4MB.
+    # ds2: 2 samples of size 1MB. Total = 2MB.
+    # ds3: 2 samples of size 3 and 5. Total = 8MB.
+    # Expected average = (4 + 2 + 8) / 6 = 2.333...
+    avg_size_mb = prefetch_autotune._get_average_element_size_mb(
+        [ds1, ds2, ds3], samples_to_check=6
+    )
+    self.assertAlmostEqual(avg_size_mb, 14 / 6)
+
+  def test_get_average_element_size_mixed_ds_mb(self):
+    element1 = np.zeros(1 * 1024 * 1024, dtype=np.uint8)  # 1MB
+    element2 = np.zeros(3 * 1024 * 1024, dtype=np.uint8)  # 3MB
+    element3 = np.zeros(5 * 1024 * 1024, dtype=np.uint8)  # 5MB
+    ds1 = dataset.MapDataset.source(
+        [element1, element2, element3]
+    ).to_iter_dataset()
+    ds2 = dataset.MapDataset.source([element1] * 10).to_iter_dataset()
+    ds3 = dataset.MapDataset.source([element2, element3] * 5).to_iter_dataset()
+    ds4 = dataset.IterDataset.mix([ds1, ds2])
+    mixed_ds = dataset.IterDataset.mix([ds3, ds4])
+
+    # samples_to_check = 5
+    # There are 3 root datasets with non-batched elements, consisting of:
+    # ds3 = [3, 5, 3, 5, 3, 5, 3, 5, 3, 5], d1 = [1, 3, 5], ds2 = [1, ... 1]
+    # The average size is (3 + 5 + 1 + 3 + 1 + 1) / 6 = 2.3364
+    performance_config = prefetch_autotune.pick_performance_config(
+        mixed_ds, ram_budget_mb=500, max_workers=None, max_buffer_size=None
+    )
+    self.assertEqual(performance_config.read_options.prefetch_buffer_size, 71)
 
 
 if __name__ == '__main__':
