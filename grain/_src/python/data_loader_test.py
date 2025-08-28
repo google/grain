11fab1359c3da5a34000cb048ceea91c9ad1fa4f
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2025-08-28 11:36:10 +0000

    TST: fix the path in data_loader_test.py
    pytest starts the paths from the "root" of the project

--- grain/_src/python/data_loader_test.py
+++ grain/_src/python/data_loader_test.py
@@ -165,7 +165,7 @@ class DataLoaderTest(absl_parameterized.TestCase):
 
   def setUp(self):
     super().setUp()
-    self.testdata_dir = pathlib.Path(FLAGS.test_srcdir) / "testdata"
+    self.testdata_dir = pathlib.Path(FLAGS.test_srcdir) / "grain" / "_src" / "python" /"testdata"
     self.read_options = (
         options.ReadOptions(num_threads=self.num_threads_per_worker)
         if (self.num_threads_per_worker is not None)
