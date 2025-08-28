a827216570842567499c0458b0abac5684c1deb6
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2025-08-28 12:07:46 +0000

    TST: run all tests with $ bash grain/oss/build_whl.sh

--- grain/oss/build_whl.sh
+++ grain/oss/build_whl.sh
@@ -125,7 +125,8 @@ main() {
 
   pushd "${OUTPUT_DIR}/all_dist"
   # TODO: remove `-k` option and execute all tests with pytest
-  $PYTHON_BIN -m pytest --pyargs grain -k "TreeJaxTest or FirstFitPackIterDatasetTest or JaxImportTest or TFImportTest or SharedMemoryArrayTest or PackingTest"
+  #$PYTHON_BIN -m pytest --pyargs grain -k "TreeJaxTest or FirstFitPackIterDatasetTest or JaxImportTest or TFImportTest or SharedMemoryArrayTest or PackingTest"
+  $PYTHON_BIN -m pytest --pyargs grain -v
   popd
 }
 
