4f32897c0315e67b801527e0623a31d9f95fde80
Nithin Tatikonda <nithintati@google.com>
2026-03-05 10:38:56 -0800

    Internal
    PiperOrigin-RevId: 879141080

---
+++ grain/_src/python/dataset/transformations/autotune.py
@@ -0,0 +1,12 @@
+"""Autotune Python library for Grain."""
+
+
+from typing import Any, TypeVar
+
+T = TypeVar("T")
+
+
+def autotune(
+    ds: Any, allow_unknown_nodes: bool = False
+) -> Any:
+  raise NotImplementedError("Autotune is not yet available.")
---
+++ grain/_src/python/dataset/transformations/autotune_test.py
@@ -0,0 +1,11 @@
+"""Autotune Python library tests."""
+
+
+import unittest
+
+class AutotuneTest(unittest.TestCase):
+  def test_autotune_not_implemented(self):
+    pass
+
+if __name__ == "__main__":
+  unittest.main()
